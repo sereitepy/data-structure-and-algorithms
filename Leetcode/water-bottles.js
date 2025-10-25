9574b5ab9ff278de41c6eda80baf6ab2b751c782
sereitepy <106307116+sereitepy@users.noreply.github.com>
2025-10-25 18:06:09 +0700

    Create water-bottles.js

---
+++ Leetcode/water-bottles.js
@@ -0,0 +1,53 @@
+/**
+ * @param {number} numBottles
+ * @param {number} numExchange
+ * @return {number}
+ */
+const numWaterBottles = function (numBottles, numExchange) {
+  let whatWeHave = numBottles
+  let drink = numBottles
+  let remainder = 0
+  
+  while (whatWeHave >= numExchange) {
+    division = whatWeHave / numExchange
+    bottle = whatWeHave % numExchange
+    total = bottle + remainder
+    whatWeHave = total
+    drink += total
+
+    let rounder = Math.round(drink)
+    if (rounder - drink > 5) {
+      console.log(Math.round(drink),
+      rounder - drink,
+      drink,
+      )
+    } else {
+      console.log(Math.floor(drink))
+    }
+
+  }
+  let rounder = Math.round(drink)
+    if (rounder - drink > 5) {
+      return Math.round(drink)
+    } else {
+      return Math.floor(drink)
+    }
+  
+}
+
+numWaterBottles(21, 4)
+
+// ----------------------------------------------------------------
+
+var corrected = function (numBottles, numExchange) {
+  let drink = numBottles // We drink all initial bottles
+  let emptyBottles = numBottles // Now we have this many empty bottles
+
+  while (emptyBottles >= numExchange) {
+    let newBottles = Math.floor(emptyBottles / numExchange) // Exchange for new bottles
+    drink += newBottles // Drink the new bottles
+    emptyBottles = (emptyBottles % numExchange) + newBottles // Leftover + newly emptied bottles
+  }
+
+  return drink
+}
