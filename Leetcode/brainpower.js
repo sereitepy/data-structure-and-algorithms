3b68c2be5d7f02a285f9fb8bcbac7dee38fcc68a
sereitepy <106307116+sereitepy@users.noreply.github.com>
2025-11-04 14:18:09 +0700

    attempt at this leetcode
    couldn't even access the data i wanted. i really need to learn more data structures

---
+++ Leetcode/brainpower.js
@@ -0,0 +1,20 @@
+function brainPowerQuestion(questions) {
+  let result = 0
+  let brainPower = 1
+  for (let i = 0; i < questions.length; i += brainPower) {
+    for (const x of questions[i]) {
+      result += questions[x][0]
+      brainPower = questions[x][1] + 0
+      console.log(questions[i], result, brainPower, questions[x])
+    }
+  }
+  console.log(result)
+}
+
+brainPowerQuestion([
+  [1, 1],
+  [2, 2],
+  [3, 3],
+  [4, 4],
+  [5, 5],
+])
